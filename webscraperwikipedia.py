5c89907175bf2f995b428b2c0f58e215d0c3e1fc
p5322 <147680302+p5322@users.noreply.github.com>
2023-10-12 15:21:58 -0400

    Update webscraperwikipedia.py
    added citation generations

--- webscraperwikipedia.py
+++ webscraperwikipedia.py
@@ -1,4 +1,23 @@
 import requests
+import pytz
+from datetime import datetime
+
+def generate_mla_citation(title, url):
+    author = "Wikipedia Contributors"
+    article_title = title
+    site_title = "Wikipedia"
+    
+    # Get the user's local timezone
+    user_timezone = pytz.timezone('America/New_York')  # Change this to the user's actual timezone
+
+    # Get the current time in the user's timezone
+    current_time = datetime.now(user_timezone)
+    
+    # Format the access date
+    access_date = current_time.strftime('%d %B %Y')  # Example format: 11 October 2023
+
+    citation = f"{author}. \"{article_title}.\" {site_title}. {access_date}.\n{url}\n"
+    return citation
 
 def search_wikipedia(query):
     # Prepare the search URL with the given query
@@ -19,14 +38,14 @@ def search_wikipedia(query):
         search_results = data["query"]["search"]
 
         if search_results:
+            print("MLA Citations:")
             for result in search_results:
                 title = result["title"]
-                page_id = result["pageid"]
 
                 # Generate the link for the article in Markdown format
                 url = f"https://en.wikipedia.org/wiki/{title.replace(' ', '_')}"
-                markdown_link = f"[{title}]({url})"
-                print(markdown_link)
+                mla_citation = generate_mla_citation(title, url)
+                print(mla_citation)
         else:
             print("No search results found.")
     else:
